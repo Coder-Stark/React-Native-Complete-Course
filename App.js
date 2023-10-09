fb02f979661cdd2360902931344e0980b4f39671
Coder-Stark <shivamkumar789321@gmail.com>
2023-10-09 12:56:21 +0530

    Lecture 05

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View, Button} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>Hello React Native</Text>
+      <Text style={{fontSize: 20}}>Hello React Native</Text>
+      <Text style={{fontSize: 15}}>Hello React Native</Text>
+      <Button title="Press Button"></Button>
+      <Button title="Press me too"></Button>
+    </View>
+  );
+};
+
+export default App;
