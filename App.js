b79ae5c8b66f33f62537ba3bdfb326c9589b4874
Coder-Stark <shivamkumar789321@gmail.com>
2023-10-09 13:25:42 +0530

    Lecture 06

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import {Text, View, Button} from 'react-native';
 
+const name = 'Shivam Kumar';
+let age = 22;
+var email = '123@gmail.com';
+
+function fruit() {
+  return 'apple';
+}
 const App = () => {
   return (
     <View>
       <Text style={{fontSize: 30}}>Hello React Native</Text>
-      <Text style={{fontSize: 20}}>Hello React Native</Text>
-      <Text style={{fontSize: 15}}>Hello React Native</Text>
-      <Button title="Press Button"></Button>
+      <Text style={{fontSize: 30}}>{name}</Text>
+      <Text style={{fontSize: 30}}>{age}</Text>
+      <Text style={{fontSize: 30}}>{email}</Text>
+      <Text style={{fontSize: 30}}>{fruit()}</Text>
+      <Text style={{fontSize: 30}}>{100 + 20}</Text>
+      <Text style={{fontSize: 30}}>{age === 22 ? 'Adult' : 'Unknown'}</Text>
       <Button title="Press me too"></Button>
     </View>
   );
