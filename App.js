81651900f290482fa716fb6a79e2f501068adc8d
Coder-Stark <shivamkumar789321@gmail.com>
2023-10-14 18:21:55 +0530

    L-10 Props_in_react_native

--- App.js
+++ App.js
@@ -3,25 +3,21 @@ import {Button, Text, View} from 'react-native';
 
 // components are always have first letter capital
 const App = () => {
-  const [name, setName] = useState("Shivam Kumar");
-  let data = "Sam"
-
-  const testName = ()=>{
-    setName("Shivam Rajbhar");
-    data = "Peter"
-    // sam should change to peter but not changed after pressing button 
-    console.warn('Sam is not change to Petter (variable not state)');
-  }
+  const [name, setName] = useState("Bruce");
   return (
     <View>
-      <Text style={{fontSize: 50}}>State in React Native</Text>
-
-      {/* after updating state, the component is  re-render */}
-      <Text style={{fontSize: 30}}>{name}</Text>
-      
-      {/* after updating variable, the component is not render */}
-      <Text style={{fontSize: 30}}>{data}</Text> 
-      <Button title='Updata Name' onPress={testName}/>
+      <Text style={{fontSize: 50}}>Props in React Native</Text>
+      <Button title='Update Name' onPress={()=>setName("Shivam")}/>
+      <User name={name} age={22}/>
+    </View>
+  );
+};
+const User = (props) => {
+  console.warn('After pressing Button => ' + props.name + ' , Age => ' + props.age);
+  return (
+    <View style={{backgroundColor: 'green', padding: 5}}>
+      <Text style={{fontSize: 30}}>Name : {props.name}</Text>
+      <Text style={{fontSize: 30}}>Age : {props.age}</Text>
     </View>
   );
 };
