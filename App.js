882c21b815355c7071d2b210885b237afd7c17c0
Coder-Stark <shivamkumar789321@gmail.com>
2023-10-18 13:47:40 +0530

    L-12 Handle_Text_Input

--- App.js
+++ App.js
@@ -1,42 +1,32 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-import ExStyles from './style';
+import React, { useState } from 'react';
+import {Text, TextInput, View, StyleSheet, Button} from 'react-native';
 
 const App = () => {
+  const [name, setName] = useState("");
   return (
     <View>
-      <Text style={{fontSize: 50}}>Styles in React Native</Text>
+      <Text style={{fontSize: 50}}>Handle Text Input</Text>
+      <Text style={{fontSize: 20}}>Your Name is : {name}</Text>
 
-      <Text style={{fontSize: 30, color:'red', backgroundColor:'yellow'}}>This is Inline Styling</Text>
-
-      <Text style={styles.textBox}>This is Internal Styling</Text>
-      <Text style={styles.textBox}>This is Internal Styling</Text>
-
-
-      <Text style={ExStyles.textBox}>This is External Styling</Text>
-      <Text style={ExStyles.textBox}>This is External Styling</Text>
-
-      <Text style={[{marginTop:20}, styles.textBox, ExStyles.textBox]}>All Styling Combinations</Text>
-
+      <TextInput 
+      style = {styles.textInput}
+      placeholder='Enter Your Name'
+      value={name}
+      onChangeText={(text)=>setName(text)}
+      />
+      <Button title='Clear Input Text' onPress={()=>setName('')}  />
     </View>
   );
 };
 
-//internal styling
 const styles = StyleSheet.create({
-  textBox:{
-    fontSize:30,
-    color:'blue',
-    backgroundColor:'gray',
-    marginBottom:10,
-    padding:5,
-    borderWidth:2,
-    borderColor:'red',
-    borderRadius:10,
-    height:50,
-    textAlignVertical:'center',
-    textAlign:'center',
-  },
-});
+  textInput : {
+    fontSize : 18,
+    color : 'blue',
+    borderColor : 'green',
+    borderWidth: 2,
+    margin: 10,
+  }
+})
 
 export default App;
