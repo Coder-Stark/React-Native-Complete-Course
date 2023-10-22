b26d9d00308f92b29a51794aeb39f944fd308636
Coder-Stark <shivamkumar789321@gmail.com>
2023-10-22 16:36:10 +0530

    L-14 List_with_FlatList_Component

--- App.js
+++ App.js
@@ -1,78 +1,46 @@
-import React, { useState }  from 'react';
-import {Text, TextInput, View, StyleSheet, Button} from 'react-native';
+import React from 'react';
+import {FlatList, Text, View, StyleSheet} from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [display, setDisplay] = useState(false);
-
-  const resetFormData = () => {
-    setName('');
-    setEmail('');
-    setPassword('');
-    setDisplay(false);
-  }
-
+  const users = [
+    {
+      id: 1,
+      name: "Shivam"
+    },
+    {
+      id: 2,
+      name: "Aditya"
+    },
+    {
+      id: 3,
+      name: "Satyam"
+    },
+    {
+      id: 4,
+      name: "Vikas"
+    }
+  ]
   return (
     <View>
-      <Text style={{fontSize: 50}}>Simple Form in React Native</Text>
-
-      <TextInput
-        style = {styles.textInput}
-        placeholder='Enter User Name'
-        value={name}
-        onChangeText={(text)=>setName(text)}
+      <Text style={{fontSize: 50}}>List With Flat List Component</Text>
+      <FlatList
+        data={users}
+        renderItem={({item})=><Text style={styles.item}>{item.name}</Text>}
+        keyExtractor={item=>item.id}
       />
-
-      <TextInput
-        style = {styles.textInput}
-        placeholder='Enter Email Id'
-        value={email}
-        onChangeText={(text)=>setEmail(text)}
-      />
-
-      <TextInput
-        style = {styles.textInput}
-        placeholder='Enter Your Password'
-        value={password}
-        onChangeText={(text)=>setPassword(text)}
-        secureTextEntry = {true}
-      />
-
-      <View style={{marginBottom:10}}>
-        <Button color={'green'} 
-        title='Show Details' 
-        onPress={()=>setDisplay(true)}
-        />
-      </View>
-
-        {/* style={{margin:10}} not allowed in below line */}
-      <Button title='Clear Details' 
-      onPress={resetFormData}
-      />
-
-      <View>
-        {
-          display ?
-          <View>
-            <Text style={{fontSize: 18}}>User Name is : {name}</Text>
-            <Text style={{fontSize: 18}}>Email Id is : {email}</Text>
-            <Text style={{fontSize: 18}}>Your Password is : {password}</Text>
-          </View>
-          : null
-        }
-      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  textInput : {
-    fontSize : 18,
-    borderColor : 'blue',
-    borderWidth: 2,
-    margin : 10
+  item:{
+    fontSize:30,
+    backgroundColor:"blue",
+    color:"white",
+    margin:10,
+    padding:10,
+    borderColor: "gray",
+    borderWidth:4
   }
 })
 
