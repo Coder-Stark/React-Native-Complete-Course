141295c7b85f81a27e4fd81551aca1d8eb8405c9
Coder-Stark <shivamkumar789321@gmail.com>
2023-10-24 13:52:38 +0530

    L-19 Class_Component_In_React_Native

--- App.js
+++ App.js
@@ -1,39 +1,23 @@
-import React from 'react';
-import {SectionList, Text, View} from 'react-native';
+import React , {Component}from 'react';
+import {Button, Text, TextInput, View} from 'react-native';
+import Students from './components/StudentsCC';
+class App extends Component {
+  fruit=()=>{
+    console.warn("Apple");
+  }
+  render(){
+    return(
+      <View>
+        <Text style={{fontSize: 50}}>Class Component In React Native</Text>
+        <TextInput
+          placeholder='Enter Your name'
+        />
+        <Button title='Press Me' onPress={this.fruit}/>
+        <Students/>
+      </View>
+    )
+  }
+}
 
-const App = () => {
-const users = [
-    {
-      id: 1,
-      name: "Shivam",
-      data:["ReactJs", "NextJs", "MongoDB", "C++"]
-    },
-    {
-      id: 2,
-      name: "Aditya",
-      data:["Business", "finance", "communicatio"]
-    },
-    {
-      id: 3,
-      name: "Satyam",
-      data:["Business", "Factory", "Dye"]
-    },
-    {
-      id: 4,
-      name: "Vikas",
-      data:["Neet", "Mbbs", "BDS"]
-    }
-  ]
-  return (
-    <View>
-      <Text style={{fontSize: 50}}>Section List in React Native</Text>
-      <SectionList 
-        sections={users}
-        renderItem={({item})=><Text style={{fontSize:20, marginLeft:20}}>{item}</Text>}
-        renderSectionHeader={({section:{name}})=><Text style={{fontSize:30, color:"red"}}>{name}</Text>}
-      />
-    </View>
-  );
-};
 
 export default App;
---
+++ components/StudentsCC.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+import {Text,View} from 'react-native';
+
+class Students extends Component {
+  render(){
+    return(
+      <View>
+        <Text style={{fontSize: 30}}>Student Class Component</Text>
+      </View>
+    )
+  }
+}
+
+export default Students;
