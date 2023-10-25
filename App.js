545806989d555263430a2969f25debc8f77adf80
Coder-Stark <shivamkumar789321@gmail.com>
2023-10-25 18:46:45 +0530

    L-23 Life_Cycle_With_UseEffect

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {Button, Text, View} from 'react-native';
 
-const App= ()=>{
+const App = ()=>{
+  const [count, setCount] = useState(0);
+
+  useEffect(()=>{
+    console.warn("Hello");
+  },[])
+
   return(
     <View>
-      <Text style={{fontSize:50}}>Hooks in React Native</Text>
-      <Text style={{fontSize:30, color:"red"}}>'use' key word defined hooks</Text>
-      <Text style={{fontSize:20}}>hooks can't be use in class based component</Text>
-      <Text style={{fontSize:30, color:"red"}}>Hooks can be used in :</Text>
-      <Text style={{fontSize:20}}>State</Text>
-      <Text style={{fontSize:20}}>Life Cycle methods</Text>
-      <Text style={{fontSize:20}}>Content Api</Text>
-      <Text style={{fontSize:20}}>Ref and Memo</Text>
-      <Text style={{fontSize:25}}>We can make Custom Hooks also</Text>
+      <Text style={{fontSize:50}}>Life Cycle with UseEffect</Text>
+      <Text style={{fontSize:20, color: "red"}}>For using life cycles methods in functional components we use useEffect hooks</Text>
+      <Text style={{fontSize:30, color:"green"}}>UseEffect for Mounting phase</Text>
+      <Text style={{fontSize:25}}>Increasing Count {count}</Text>
+      <Button title='Update Count' onPress={()=>setCount(count+1)}/>
+      <Text style={{fontSize:25}}>The warn is only first time  with using this '[ ]' in useEffect hook otherwise warn every time</Text>
+      <Text style={{fontSize:25}}>or we can say work as componentDidMount in class component</Text>
     </View>
   );
 }
