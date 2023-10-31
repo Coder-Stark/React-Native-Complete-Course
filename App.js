b141f8e3f470e78ead80b0ecd6826cdf607b041b
Coder-Stark <shivamkumar789321@gmail.com>
2023-10-31 12:30:33 +0530

    L-25 Show_Hid_Or_Toggle_Component

--- App.js
+++ App.js
@@ -1,47 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {Button, Text, View} from 'react-native';
 
 const App = ()=>{
-  const[count, setCount] = useState(0);
-  const[data, setData] = useState(100);
-
-  //useEffect on states
-  useEffect(()=>{
-    console.warn("When Count State is Updated");
-  },[count]);                            //if [count] is not there then this effect work on both buttons for multiple we can use [count, name ,age]
-
-  useEffect(()=>{
-    console.warn("When Data State is Updated");
-  },[data]);
+  const [show, setShow] = useState(false);
   return(
     <View>
-      <Text style={{fontSize:50}}>Life Cycle with UseEffect</Text>
-      <Text style={{fontSize:20, color: "red"}}>For using life cycles methods in functional components we use useEffect hooks</Text>
-      <Text style={{fontSize:30, color:"green"}}>UseEffect as CompononetDidUpdate | {count} | {data}</Text>
-      <Text style={{fontSize:30, color:"blue"}}>UseEffect on States</Text>
-      <Text style={{fontSize:30, color: "violet"}}>Count : {count}</Text>
-      <Text style={{fontSize:30, color: "violet"}}>Data : {data}</Text>
-      <Button title='Update Count' onPress={()=>setCount(count+1)} color={'green'} />
-      <Button title='Update Data' onPress={()=>setData(data+1)}/>
-
-      <User info={{count, data}}/>
+      <Text style={{fontSize:50}}>Show/Hide or Toggle Component</Text>
+      <Button title='Toggle Component' onPress={()=>setShow(!show)}/>
+      {
+        show ? <User/> : null
+      }
     </View>
   );
 }
 
-const User = (props) =>{
-  useEffect(()=>{
-    console.warn("When Count Props is Updated");
-  },[props.info.count]);
-
-  useEffect(()=>{
-    console.warn("When Data Props is Updated");
-  },[props.info.data]);
+//after hiding it means it will Unmount Life cycle method
+const User = () => {
   return(
     <View>
-      <Text style={{fontSize:30, color: "orange"}}>UseEffect on Props</Text>
-      <Text style={{fontSize:30, color: "violet"}}>Count : {props.info.count}</Text>
-      <Text style={{fontSize:30, color: "violet"}}>Data : {props.info.data}</Text>
+      <Text style={{fontSize: 30, color: "orange", margin: 20 , textAlign:'center'}}>User component shows after Clicking Toggle Button</Text>
     </View>
   );
 }
