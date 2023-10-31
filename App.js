867dc7fc72155c4c4c24f1b7521afb7f5fae2b91
Coder-Stark <shivamkumar789321@gmail.com>
2023-10-31 13:31:43 +0530

    L-26 UseEffect_For_Unmount_Component__IMPORTANT

--- App.js
+++ App.js
@@ -1,24 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {Button, Text, View} from 'react-native';
 
 const App = ()=>{
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(true);
   return(
     <View>
-      <Text style={{fontSize:50}}>Show/Hide or Toggle Component</Text>
+      <Text style={{fontSize:50}}>UseEffect For Unmount Component</Text>
       <Button title='Toggle Component' onPress={()=>setShow(!show)}/>
       {
-        show ? <User/> : null
+        show ? <Student/> : null
       }
     </View>
   );
 }
 
 //after hiding it means it will Unmount Life cycle method
-const User = () => {
+const Student = () => {
+  let timer = setInterval(()=>{
+    console.warn("Timer Called");
+  }, 2000)
+
+  useEffect(()=>{
+    // console.warn("UseEffect Call on Unmount");                    //reverse of below(when shows then warn)
+    // return ()=>{
+    //   console.warn("UseEffect Call on Unmount");                    //when not shows(unmount) then warn
+    // }    
+    return()=>clearInterval(timer);                                   //for stop background usage
+  });
+
   return(
     <View>
-      <Text style={{fontSize: 30, color: "orange", margin: 20 , textAlign:'center'}}>User component shows after Clicking Toggle Button</Text>
+      <Text style={{fontSize: 30, color: "orange", margin: 20 , textAlign:'center'}}>Student component shows after Clicking Toggle Button</Text>
+      <Text style={{fontSize: 30, color: "violet", margin: 20 , textAlign:'center'}}>If again Click on Toggle button The Timer is also Stops</Text>
     </View>
   );
 }
