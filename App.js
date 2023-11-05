41286a127d27233004b6e0fb8d056246d352e539
Coder-Stark <shivamkumar789321@gmail.com>
2023-11-05 13:15:01 +0530

    L-28 TouchableHighLight_Or_TouchableOpacity

--- App.js
+++ App.js
@@ -1,54 +1,57 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, TouchableHighlight, View, StyleSheet} from 'react-native';
 
 const App = ()=>{
   return(
     <View style={styles.main}>
-      <Text style={{fontSize:50}}>Responsive UI with Flex</Text>
-      {/* <View style={{flex:1, backgroundColor: "orange"}}></View> */}
-      <View style={styles.box1}></View>
-      <View style={styles.box2}>
-        <View style={styles.innerBox1}></View>
-        <View style={styles.innerBox2}></View>
-        <View style={styles.innerBox3}></View>
-      </View>
-      <View style={styles.box3}></View>
+      {/* default button have not custom styling so we use belows method */}
+      <Text style={{fontSize:44}}>TouchableHighLight Or TouchableOpacity</Text>
+        <TouchableHighlight>
+          <Text style={styles.button}>Button</Text>
+        </TouchableHighlight>
+        <TouchableHighlight>
+          <Text style={[styles.button, styles.success]}>Success</Text>
+        </TouchableHighlight>
+        <TouchableHighlight>
+          <Text style={[styles.button, styles.primary]}>Primary</Text>
+        </TouchableHighlight>
+        <TouchableHighlight>
+          <Text style={[styles.button, styles.warning]}>Warning</Text>
+        </TouchableHighlight>
+        <TouchableHighlight>
+          <Text style={[styles.button, styles.error]}>Error</Text>
+        </TouchableHighlight>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   main:{
-    flex : 1,
-    // flexDirection: "row"
+    flex:1
   },
-  box1 : {
-    flex : 1,
-    backgroundColor: "orange"
-  },
-  box2 : {
-    flex : 1,
-    backgroundColor: "white",
-    flexDirection:"row"
-  },
-  box3 : {
-    flex : 1,
-    backgroundColor: "green"
-  },
-  innerBox1:{
-    flex:1,
-    backgroundColor:"red",
-    margin:10
-  },
-  innerBox2:{
-    flex:1,
-    backgroundColor:"blue",
+  button:{
+    backgroundColor:"gray",
+    color: "white",
+    fontSize:25,
+    textAlign:"center",
+    padding:5,
     margin:10,
+    borderRadius:10,
+    shadowColor:"black",
+    elevation:10,
+    shadowOpacity:1,
   },
-  innerBox3:{
-    flex:1,
-    backgroundColor:"black",
-    margin:10
+  success:{
+    backgroundColor:"green"
+  },
+  primary:{
+    backgroundColor:"blue"
+  },
+  warning:{
+    backgroundColor:"gold"
+  },
+  error:{
+    backgroundColor:"red"
   }
 })
 
