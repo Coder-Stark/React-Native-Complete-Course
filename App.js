8d79959ad890398747a3e0752fe920da017d6cfa
Coder-Stark <shivamkumar789321@gmail.com>
2023-11-15 19:25:12 +0530

    L-37 Custom_Modals_Or_DialogBox

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-import {WebView} from 'react-native-webview';
+import React , {useState} from 'react';
+import {Text, View, StyleSheet, Button} from 'react-native';
 
 const App = ()=>{
+  const [show, setShow] = useState(false);
   return(
     <View style={styles.main}>
       <View>
-        <Text style={{fontSize:50}}>Install Package In React Native</Text>
+        <Text style={{fontSize:50}}>Custom Modals Or DialogBox</Text>
       </View>
       <View style={styles.content}>
-        <WebView
-          source={{uri: 'https://linktr.ee/shivam__kumar'}}
-        />
+      {
+        show ? 
+        <View style={styles.modal}>
+          <View style={styles.body}>
+            <Text style={styles.text}>Click to Disappear</Text>
+            <Button title='Close Modal' onPress={()=>setShow(false)}/>
+          </View>
+        </View> :
+        null
+      }
+      <Button title='Open DialogBox' onPress={()=>setShow(true)}/>
       </View>
     </View>
   );
@@ -23,6 +31,25 @@ const styles = StyleSheet.create({
   },
   content:{
     flex:1,
+    justifyContent:"flex-end"
+  },
+  modal:{
+    flex:1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  body:{
+    height:150,
+    width:300,
+    padding:20,
+    backgroundColor:'white',
+    justifyContent:'flex-end',
+    borderRadius:20,
+  },
+  text:{
+    fontSize:30,
+    textAlign:'center',
   }
 })
 export default App;
