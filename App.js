650067799950fa685329e2e2e9f3fb05699c86cb
Coder-Stark <shivamkumar789321@gmail.com>
2023-11-25 07:55:55 +0530

    L-41 Button_and_Component_In_Stack_Navigation

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {Text, View, StyleSheet, Button} from 'react-native';
+import {Text, View, StyleSheet, Button, TextInput} from 'react-native';
 import {NavigationContainer} from "@react-navigation/native"
 import {createNativeStackNavigator} from "@react-navigation/native-stack"
 
 const Stack = createNativeStackNavigator();
 const App = ()=>{
+  const btnAction = ()=>{
+    console.warn("Button Pressed")
+  }
   return(
     <NavigationContainer>
       <Stack.Navigator
@@ -14,6 +17,9 @@ const App = ()=>{
             headerStyle:{
               backgroundColor:"orange"
             },
+            headerTitle:()=><Button title='Left' onPress= {btnAction}/>,        //for adding in left part of navbar
+            // headerRight:()=><Button title='Right' onPress={btnAction}/>,        //for adding in right part of navbar
+            headerRight:()=><Search/>,                                                    //for calling component
             headerTitleStyle:{
               color:"white",
               fontSize:30,
@@ -37,9 +43,15 @@ const App = ()=>{
   );
 }
 
+const Search = ()=>{
+  return(
+    <TextInput placeholder='Search' placeholderTextColor={"white"}/>
+  )
+}
 const Home = (props)=>{                         //some props are already present in navigation
   return(
     <View style={styles.homeView}>
+      <Text style={{fontSize:30}}>Button and Component in Stack Navigation</Text>
       <Text style={{fontSize:40}}>Home Screen</Text>
       <Button title="Move to Login" onPress={()=>props.navigation.navigate("Login")}/>
     </View>
