7e97c03ef1fb7ce5a65036476286bd897f90dd01
Coder-Stark <shivamkumar789321@gmail.com>
2023-12-09 20:16:21 +0530

    L-47 List_With_API_Call

--- App.js
+++ App.js
@@ -1,31 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import {Text, View} from 'react-native';
+import {ScrollView, Text, View} from 'react-native';
 const App = ()=>{
-  const [data, setData] = useState(undefined);
-  const getAPIData = async()=>{
-    //api call
-    const url = "https://jsonplaceholder.typicode.com/posts/1";
+  const [data, setData] = useState([]);
+  const getAPIData = async ()=>{
+    const url = "https://jsonplaceholder.typicode.com/posts";
     let result = await fetch(url);
     result = await result.json();
-    // console.warn(result);
     setData(result);
-  }
+  };
   useEffect(()=>{
     getAPIData();
-  },[]);                        //[] = for calling only once
+  },[]);
+
   return(
-    <View>
-      <Text style={{fontSize:50, textAlign:"center"}}>API Call</Text>
+    <ScrollView>
+      <Text style={{fontSize:50, textAlign:"center"}}>List With API Call</Text>
+      <Text style={{fontSize:25, padding: 5 , margin:5, fontWeight:"bold"}}>Fetched Data</Text>
       {
-        data ? <View>
-          <Text style={{fontSize:25, padding: 5 , margin:5, fontWeight:"bold"}}>Fetched Data</Text>
-          <Text style={{fontSize:25, padding: 5 , margin:5}}>ID : {data.id}</Text>
-          <Text style={{fontSize:25, padding: 5 , margin:5}}>UserID : {data.userId}</Text>
-          <Text style={{fontSize:25, padding: 5 , margin:5}}>Title : {data.title}</Text>
-          <Text style={{fontSize:25, padding: 5 , margin:5}}>Body : {data.body}</Text>
-        </View> : null
+        data.length ? data.map((item)=>{
+          return(
+            <View style={{padding: 5 , margin:5, borderBottomColor:"black", borderBottomWidth:1}}>
+              {/* <Text style={{fontSize:20}}>UserId : {item.userId}</Text> */}
+              <Text style={{fontSize:20, backgroundColor:"gray"}}>Id : {item.id}</Text>
+              <Text style={{fontSize:20}}>Title : {item.title}</Text>
+              <Text style={{fontSize:20}}>Body : {item.body}</Text>
+            </View>
+          )
+        }):
+        null
       }
-    </View>
+    </ScrollView>
   );
 }
 
