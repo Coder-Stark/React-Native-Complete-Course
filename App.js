7bcf5ec074554dfb00eeba8609700cb1f05645c3
Coder-Stark <shivamkumar789321@gmail.com>
2023-12-14 09:01:02 +0530

    L-50 Test_API_With_PostMan

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ import {Text, View} from 'react-native';
 const App = ()=>{
   return(
     <View>
-      <Text style={{fontSize:50, textAlign:"center"}}>Install Json Server </Text>
-      <Text style={{fontSize:30, margin:10}}>URL : https://www.npmjs.com/package/json-server</Text>
-      <Text style={{fontSize:30, margin:10}}>Package : npm install -g json-server</Text>
-      <Text style={{fontSize:30, margin:10}}>Create a Folder(API) and create a db.json file</Text>
-      <Text style={{fontSize:30, margin:10}}>Start server in folder with command : json-server --watch db.json</Text>
-      <Text style={{fontSize:30, margin:10}}>Server Started on local host 3000</Text>
+      <Text style={{fontSize:50, textAlign:"center"}}>Test API with PostMan</Text>
+      <Text style={{fontSize:40, margin:10, color:"red"}}>Most Used Methods</Text>
+      <Text style={{fontSize:30, margin:10}}>GET - For Fetching Data</Text>
+      <Text style={{fontSize:30, margin:10}}>POST - For Posting Data</Text>
+      <Text style={{fontSize:30, margin:10}}>PUT - For Updating</Text>
+      <Text style={{fontSize:30, margin:10}}>DELETE - For Deleting</Text>
     </View>
   );
 }
