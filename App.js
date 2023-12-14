803f268c40b8b469f14ef3afc068e028c535c856
Coder-Stark <shivamkumar789321@gmail.com>
2023-12-14 11:51:29 +0530

    L-51 Call_Json_Server_API

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Text, View} from 'react-native';
 const App = ()=>{
+  const [data, setData] = useState([]);
+  const getAPIdata = async()=>{
+    // console.warn("called");
+    const url = "http://10.0.2.2:3000/users";                 //replaces with http://192.168.0.103:3000/users   {connect emulator with local ip address}
+    let result = await fetch(url);
+    result = await result.json();
+    setData(result);
+  }
+  useEffect(()=>{
+    getAPIdata();
+  },[])
   return(
     <View>
-      <Text style={{fontSize:50, textAlign:"center"}}>Test API with PostMan</Text>
-      <Text style={{fontSize:40, margin:10, color:"red"}}>Most Used Methods</Text>
-      <Text style={{fontSize:30, margin:10}}>GET - For Fetching Data</Text>
-      <Text style={{fontSize:30, margin:10}}>POST - For Posting Data</Text>
-      <Text style={{fontSize:30, margin:10}}>PUT - For Updating</Text>
-      <Text style={{fontSize:30, margin:10}}>DELETE - For Deleting</Text>
+      <Text style={{fontSize:50, textAlign:"center"}}>Call Json Server API</Text>
+      {
+        data.length ? 
+        data.map((item)=>{
+          return(
+            <View style={{padding:10, borderColor:"black", borderWidth:1}}>
+              <Text style={{fontSize:30}}>Name: {item.name}</Text>
+              <Text style={{fontSize:30}}>Age: {item.age}</Text>
+              <Text style={{fontSize:30}}>Email: {item.email}</Text>
+            </View>
+          )
+        }):
+        null
+      }
     </View>
   );
 }
