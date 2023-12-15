5fed9c4a2b9fa0cd658bc196126679251f7667ce
Coder-Stark <shivamkumar789321@gmail.com>
2023-12-15 11:25:33 +0530

    L-53 POST_API_With_Input_Data

--- App.js
+++ App.js
@@ -1,37 +1,65 @@
-import React, { useEffect, useState } from 'react';
-import {Button, Text, View} from 'react-native';
+import React, { useState } from 'react';
+import {Button, Text, View, StyleSheet, TextInput} from 'react-native';
 // const url = "http://10.0.2.2:3000/users";                 //replaces with http://192.168.0.103:3000/users   {connect emulator with local ip address}
 const App = ()=>{
-  const saveData = async () => {
-    try {
-      console.warn("test");
-      const data = { name: "John Doe", age: 30, email: "zxt@gmail.com" };
+    const [name, setName] = useState('');
+    const [age, setAge] = useState(0);
+    const [email, setEmail] = useState('');
+
+    const saveData = async ()=>{
+      console.warn(name);
       const url = "http://10.0.2.2:3000/users";
       let result = await fetch(url, {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data)
+        // body: JSON.stringify({name: name, age: age,email: email})       //same as below
+        body: JSON.stringify({name, age, email})
       });
-  
-      if (!result.ok) {
-        throw new Error('Failed to save data');
+      result = await result.json();
+      if(result){
+        console.warn("Data saved successfully");
       }
-      const responseJson = await result.json();
-      console.warn("Data Saved to DataBase", responseJson);
-    } catch (error) {
-      console.error("Error:", error);
     }
-  }
-  
-
-  return(
+    return(
     <View>
-      <Text style={{fontSize:50, textAlign:"center"}}>Simple POST API</Text>
-      <Button title="Save Data" onPress={saveData}/>
+      <Text style={{fontSize:50, textAlign:"center"}}>POST API With Input Data</Text>
+      <TextInput
+        style={styles.input}
+        value= {name}
+        onChangeText={text => setName(text)}
+        type="text" 
+        placeholder="Enter Name" 
+      />
+
+      <TextInput
+        style={styles.input}
+        value= {age}
+        onChangeText={text => setAge(text)}
+        type="text"
+        placeholder="Enter Age" 
+      />
+
+      <TextInput
+        style={styles.input}
+        value= {email}
+        onChangeText={text => setEmail(text)}
+        type="text"
+        placeholder="Enter Email"
+      />
+      <Button title = "Save Data" onPress={() => saveData()} />
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  input:{
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize:20
+  }
+})
 export default App;
