5597542eeb5880c6a2deac2100e01307591cc97c
Coder-Stark <shivamkumar789321@gmail.com>
2023-12-22 11:16:17 +0530

    L-65 Element_Inspector

--- App.js
+++ App.js
@@ -1,33 +1,27 @@
-import React, { useState } from 'react';
-import {Button, Text, View} from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
 const App = ()=>{
-  const [user, setUser] = useState('');
-  const setData = async()=>{
-    // await AsyncStorage.setItem('key', 'value');
-    await AsyncStorage.setItem('user', 'Shivam Kumar');
-    console.warn("Data Setted(SET DATA) then after (GET DATA)")
-  }
-  const getData = async()=>{
-    const name = await AsyncStorage.getItem('user');
-    // console.warn(name);
-    setUser(name);
-  }
-  const removeData = async()=>{
-    await AsyncStorage.removeItem('user');
-    setUser('');
-    console.warn("Data Removed");
-  }
+
   return(
     <View>
-      <Text style={{fontSize:50}}>Async Storage With React Native</Text>
-      <Text style={{fontSize:20, color:'red'}}>Asynchronous storage refers to a mechanism for storing and persisting data asynchronously on a user's device. It allows developers to save data locally on the device's storage in an asynchronous manner, meaning that the data operations don't block the main thread or user interface, ensuring a smooth user experience.</Text>
-      <Text style={{fontSize:30, borderColor:'green', borderWidth:2, margin:5, padding:5}}>User : {user}</Text>
-      <Button title = "Set Data" onPress={setData} />
-      <Button title = "Get Data" onPress={getData} />
-      <Button title = "Remove Data" onPress={removeData} />
+      <Text style={{fontSize:50}}>Element Inspector</Text>
+      <Text style={{fontSize:30, color:'red'}}>For Opening Inspector: </Text>
+      <Text style={{fontSize:30, color:'red'}}>Press Ctrl + m</Text>
+      <View style={styles.box}>
+
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  box:{
+    width:100,
+    height:100,
+    backgroundColor:'green',
+    margin:10,
+    padding:20
+  }
+})
 
 export default App;
