d0e6d491cd474369150a817b456b9e0d982ba9a1
Coder-Stark <shivamkumar789321@gmail.com>
2023-12-23 10:01:16 +0530

    L-68 Redux_Installation_and_File_Structure

--- App.js
+++ App.js
@@ -4,14 +4,17 @@ const App = ()=>{
 
   return(
     <View>
-      <Text style={{fontSize:50, textAlign:'center'}}>Redux Architecture</Text>
-      <Text style={{fontSize:40, color:'orange'}}>Redux Flow</Text>
-      <Text style={{fontSize:25, color:'green'}}>View</Text>
-      <Text style={{fontSize:25, color:'green'}}>Action</Text>
-      <Text style={{fontSize:25, color:'green'}}>Reducers</Text>
-      <Text style={{fontSize:25, color:'green'}}>Store </Text>
-      <Text style={{fontSize:25, color:'green'}}>View</Text>
-      <Text style={{fontSize:30, color:'orange'}}>In Saga flow after action there is another box or component to handle asynchronous action of API Calls</Text>
+      <Text style={{fontSize:50, textAlign:'center'}}>Redux Installation & File Structure</Text>
+      <Text style={{fontSize:40, color:'orange'}}>Install Redux Package</Text>
+      <Text style={{fontSize:25, color:'green'}}>-redux = npm i redux</Text>
+      <Text style={{fontSize:25, color:'green'}}>-react-redux = npm i react-redux</Text>
+      <Text style={{fontSize:25, color:'green'}}>-redux toolkit = npm i @reduxjs/toolkit</Text>
+      <Text style={{fontSize:40, color:'orange'}}>File Structure</Text>
+      <Text style={{fontSize:25, color:'green'}}>/components/-/redux</Text>
+      <Text style={{fontSize:25, color:'green'}}>/redux/action.js</Text>
+      <Text style={{fontSize:25, color:'green'}}>/redux/reducer.js</Text>
+      <Text style={{fontSize:25, color:'green'}}>/redux/store.js</Text>
+      <Text style={{fontSize:25, color:'green'}}>/redux/rootReducer.js</Text>
     </View>
   );
 }
