06b9b78ddf7d0648dd86721b0e25549d01e51869
Coder-Stark <shivamkumar789321@gmail.com>
2023-12-24 12:16:46 +0530

    L-71 UI_For_Add_To_Cart_With_Redux_Make_Reducer

---
+++ components/redux/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import {reducer} from './reducer'
+
+// export const rootReducer = combineReducers({
+//     reducer
+// })
+
+export default combineReducers({
+    reducer
+})
