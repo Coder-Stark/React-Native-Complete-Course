210773db2c7ca296b4aae4f4d882455d76e40e43
Coder-Stark <shivamkumar789321@gmail.com>
2023-12-26 09:14:58 +0530

    L-77 Add_Saga_In_Redux

--- components/redux/store.js
+++ components/redux/store.js
@@ -3,6 +3,7 @@ import rootReducer from './rootReducer';
 import createSagaMiddleware from 'redux-saga'
 import SagaData from './saga'
 
+//error 'getState' of Undefined
 const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
     reducer: rootReducer,
