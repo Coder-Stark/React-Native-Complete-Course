c9f922563149b4178d5b558ba6b0de828c988e9f
Coder-Stark <shivamkumar789321@gmail.com>
2023-12-26 18:44:40 +0530

    L-79 Render_Data_From_Redux  Course Is Completed

--- components/UserList.js
+++ components/UserList.js
@@ -5,16 +5,23 @@ import { getUserList } from './redux/action';
 
 const UserList = ()=>{
   const dispatch = useDispatch();
-  const userList = useSelector((state)=>state.reducer);
+  // const userList = useSelector((state)=>state.reducer);
+  const userList = useSelector(state=>state.reducer[0].users);
   useEffect(()=>{
     dispatch(getUserList());
-  })
+  },[])
   console.warn('In Component', userList);
   return(
-    <View>
-      <View>
-        <Text style={{fontSize:25, textAlign:'center', padding:5, justifyContent:'center'}}>User List Screen</Text>
-      </View>
+    <View style={{flex:1}}>
+      {
+        userList.length ?
+        userList.map((item, index) => (
+          <Text key={index} style={{ fontSize: 25, textAlign: 'center', padding: 5 }}>
+            Name: {item.name}, Gender: {item.gender}
+          </Text>
+        ))
+        : null
+      }
     </View>
   );
 }
